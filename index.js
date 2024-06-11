52204b247b3a1f8aadd2abbacddb79927a91871c
Yehor Heiko <yehorheiko.developer@gmail.com>
2024-06-11 14:13:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const port = 5010;
+const { log } = require('console');
+const { readFileSync } = require('fs');
+const http = require('http');
+const { parse } = require('querystring');
+const server = new http.Server
+const icon = readFileSync('favicon.ico')
+const users = [{login: 'admin', password: '123'}]
+
+server.listen(port, () => {
+    log('http://localhost:' + port)
+})
+
+
+server.addListener('request', handleRequest)
+
+
+async function handleRequest(request, response) {
+    log(request.method, request.url)
+    
+    if (request.method == 'POST') {
+        const body = await getBody(request);
+        const payload = parse(body)
+        users.push(payload)
+        log(payload)
+    }
+
+    if (request.url == '/favicon.ico') {
+        response.setHeader('Content-Type', 'image/x-icon')
+        response.end(icon)
+    }
+    else {
+        const html = readFileSync('index.html')
+        response.setHeader('Content-Type', 'text/html')
+        response.end(renderUsers(html))
+    }
+    
+}
+
+async function getBody(request) {
+    let body = ''
+    for await (const chunk of request) body += chunk
+    return body
+}
+
+function renderUsers(html) {
+    return html.toString().replace('{users}', users.map((user) => `<li>${user.login}</li>`).join(''))
+}
