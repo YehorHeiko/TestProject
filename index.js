66763f73ade278369be07ae442e29d422e79838c
Yehor Heiko <yehorheiko.developer@gmail.com>
2024-11-13 20:20:08 +0200

    Parse

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
-const port = 5010;
-const { log } = require('console');
-const { readFileSync } = require('fs');
-const http = require('http');
-const { parse } = require('querystring');
-const server = new http.Server
-const icon = readFileSync('favicon.ico')
-const users = [{login: 'admin', password: '123'}]
+// const port = 5010;
+// const { log } = require('console');
+// const { readFileSync } = require('fs');
+// const http = require('http');
+// const { parse } = require('querystring');
+// const server = new http.Server
+// const icon = readFileSync('favicon.ico')
+// const users = [{login: 'admin', password: '123'}]
 
-server.listen(port, () => {
-    log('http://localhost:' + port)
-})
-
-
-server.addListener('request', handleRequest)
+// server.listen(port, () => {
+//     log('http://localhost:' + port)
+// })
 
 
-async function handleRequest(request, response) {
-    log(request.method, request.url)
-    
-    if (request.method == 'POST') {
-        const body = await getBody(request);
-        const payload = parse(body)
-        users.push(payload)
-        log(payload)
+// server.addListener('request', handleRequest)
+
+
+// async function handleRequest(request, response) {
+//     log(request.method, request.url)
+
+//     if (request.method == 'POST') {
+//         const body = await getBody(request);
+//         const payload = parse(body)
+//         users.push(payload)
+//         log(payload)
+//     }
+
+//     if (request.url == '/favicon.ico') {
+//         response.setHeader('Content-Type', 'image/x-icon')
+//         response.end(icon)
+//     }
+//     else {
+//         const html = readFileSync('index.html')
+//         response.setHeader('Content-Type', 'text/html')
+//         response.end(renderUsers(html))
+//     }
+
+// }
+
+// async function getBody(request) {
+//     let body = ''
+//     for await (const chunk of request) body += chunk
+//     return body
+// }
+
+// function renderUsers(html) {
+//     return html.toString().replace('{users}', users.map((user) => `<li>${user.login}</li>`).join(''))
+// }
+async function getUsers(names) {
+    let jobs = [];
+
+    for (let name of names) {
+        let job = fetch(`https://api.github.com/users/${name}`)
+        .then(Response => {return Response.json()});
+        jobs.push(job);
     }
 
-    if (request.url == '/favicon.ico') {
-        response.setHeader('Content-Type', 'image/x-icon')
-        response.end(icon)
-    }
-    else {
-        const html = readFileSync('index.html')
-        response.setHeader('Content-Type', 'text/html')
-        response.end(renderUsers(html))
-    }
-    
-}
+    let results = await Promise.all(jobs);
 
-async function getBody(request) {
-    let body = ''
-    for await (const chunk of request) body += chunk
-    return body
-}
-
-function renderUsers(html) {
-    return html.toString().replace('{users}', users.map((user) => `<li>${user.login}</li>`).join(''))
+    return results;
 }
